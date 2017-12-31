b181059b90353911662287fb5b2d221d0d6ca9bb
Xingxian <john.koven@gmail.com>
2017-12-31 15:39:33 -0500

    Day 1

---
+++ src/DayOne.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DayOne {
+
+	public static int printAnswerGeneric(String input, Character lastChar){//, Character firstChar) {
+		int sum=0;
+		Character currentChar;
+		Character previousChar = lastChar;
+		
+		for (int i=0; i<input.length(); i++){
+			currentChar = input.charAt(i);
+			if (currentChar == previousChar){
+				sum = sum+(Integer.parseInt(currentChar.toString()));
+			}
+			previousChar = currentChar;
+		}
+		return sum;
+	}
+
+
+
+	public static int printAnswerSingleInput(String input) {
+		return printAnswerGeneric(input, input.charAt(input.length()-1));
+//		return printAnswerGeneric(input, input.charAt(input.length()-1), input.charAt(0));
+	}
+
+	
+}
