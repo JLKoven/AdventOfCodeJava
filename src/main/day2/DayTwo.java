65e47666b797d2bb1447ac6e7398ef6ad5f4a236
Xingxian <john.koven@gmail.com>
2018-02-07 06:02:08 -0500

    better

--- src/main/day2/DayTwo.java
+++ src/main/day2/DayTwo.java
@@ -162,65 +162,22 @@ public class DayTwo {
 
 
 	public static List<int[]> getStandardInputDayTwoPartOneAndTwo(URI fileName) {
+		List<int[]> listOfArrays = new ArrayList<int[]>();
 		List<String> linesAsString = new ArrayList<String>();
 		try {
 			linesAsString = Files.readAllLines(Paths.get(fileName));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		List<Integer> lines = new ArrayList();
-//		lines.addAll(linesAsString.stream().map(Integer::valueOf).collect(Collectors.toList()));;
-//		return lines;
-		System.out.println("lines is size "+linesAsString.size()+".");
-		System.out.println("lines[0] is "+linesAsString.get(0)+".");
-		//turn array of integers into an 
-		int[] sampleRow1 = new int[] { 1236, 741, 557, 1029, 144, 101, 1968, 2159, 1399, 80, 1139, 1167, 1695, 82, 90,
-				2236 };
-		int[] sampleRow2 = new int[] { 2134, 106, 107, 1025, 584, 619, 191, 496, 80, 352, 351, 2267, 1983, 1973, 97,
-				1244 };
-		int[] sampleRow3 = new int[] { 3227, 179, 691, 3177, 172, 1636, 3781, 2020, 3339, 2337, 189, 3516, 1500, 176,
-				159, 3279 };
-		int[] sampleRow4 = new int[] { 201, 688, 364, 180, 586, 659, 623, 577, 188, 265, 403, 670, 195, 720, 115, 37 };
-		int[] sampleRow5 = new int[] { 1892, 1664, 2737, 2676, 849, 2514, 923, 171, 311, 218, 255, 2787, 1271, 188,
-				1278, 2834 };
-		int[] sampleRow6 = new int[] { 150, 3276, 204, 603, 3130, 587, 3363, 3306, 2890, 127, 176, 174, 383, 3309, 213,
-				1620 };
-		int[] sampleRow7 = new int[] { 5903, 3686, 200, 230, 6040, 4675, 6266, 179, 5375, 1069, 283, 82, 6210, 6626,
-				6398, 1954 };
-		int[] sampleRow8 = new int[] { 942, 2324, 1901, 213, 125, 2518, 655, 189, 2499, 160, 2841, 2646, 198, 173, 1841,
-				200 };
-		int[] sampleRow9 = new int[] { 232, 45, 272, 280, 44, 248, 50, 266, 296, 297, 236, 254, 58, 212, 276, 48 };
-		int[] sampleRow10 = new int[] { 563, 768, 124, 267, 153, 622, 199, 591, 204, 125, 93, 656, 198, 164, 797, 506 };
-		int[] sampleRow11 = new int[] { 243, 4746, 1785, 204, 568, 4228, 2701, 4303, 188, 4148, 4831, 1557, 4692, 166,
-				4210, 3656 };
-		int[] sampleRow12 = new int[] { 72, 514, 1572, 172, 1197, 750, 1392, 1647, 1587, 183, 1484, 213, 1614, 718, 177,
-				622 };
-		int[] sampleRow13 = new int[] { 1117, 97, 2758, 2484, 941, 1854, 1074, 264, 2494, 83, 1434, 96, 2067, 2825,
-				2160, 92 };
-		int[] sampleRow14 = new int[] { 2610, 1290, 204, 2265, 1374, 2581, 185, 852, 207, 175, 3308, 1500, 2898, 1120,
-				1892, 3074 };
-		int[] sampleRow15 = new int[] { 2322, 1434, 301, 2156, 98, 2194, 587, 1416, 1521, 94, 1985, 424, 91, 119, 1869,
-				1073 };
-		int[] sampleRow16 = new int[] { 66, 87, 176, 107, 2791, 109, 21, 92, 3016, 2239, 1708, 3175, 3210, 2842, 446,
-				484 };
-		List<int[]> listOfArrays = new ArrayList<int[]>();
-		listOfArrays.add(sampleRow1);
-		listOfArrays.add(sampleRow2);
-		listOfArrays.add(sampleRow3);
-		listOfArrays.add(sampleRow4);
-		listOfArrays.add(sampleRow5);
-		listOfArrays.add(sampleRow6);
-		listOfArrays.add(sampleRow7);
-		listOfArrays.add(sampleRow8);
-		listOfArrays.add(sampleRow9);
-		listOfArrays.add(sampleRow10);
-		listOfArrays.add(sampleRow11);
-		listOfArrays.add(sampleRow12);
-		listOfArrays.add(sampleRow13);
-		listOfArrays.add(sampleRow14);
-		listOfArrays.add(sampleRow15);
-		listOfArrays.add(sampleRow16);
+		for (String line : linesAsString){
+			String[] parts = line.split("\\s+");
+			int[] sampleRow = new int[parts.length];
+			for (int i=0; i<parts.length; i++){
+				sampleRow[i] = Integer.parseInt(parts[i]);
+			}
+			listOfArrays.add(sampleRow);
+		}
+
 		return listOfArrays;
 	}
 
